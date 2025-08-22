9cf38b1351139bf4a5cdf301f221411b7178b9c9
Dane Hoover <DaneHoov22@gmail.com>
2025-08-22 10:47:15 -0500

    .env created and config/index.js updated. Still need JWT_SECRET for .env

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,13 @@
 // backend/config/index.js
+const path = require('path');
+require('dotenv').config();
+
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 8000,
-    dbFile: process.env.DB_FILE,
-    jwtConfig: {
-      secret: process.env.JWT_SECRET,
-      expiresIn: process.env.JWT_EXPIRES_IN
-    }
-  };
+  environment: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 8000,
+  dbFile: process.env.DB_FILE || path.resolve(__dirname, '..', 'db', 'dev.sqlite'),
+  jwtConfig: {
+    secret: process.env.JWT_SECRET || 'superSecretKey',
+    expiresIn: process.env.JWT_EXPIRES_IN || 604800
+  }
+};
